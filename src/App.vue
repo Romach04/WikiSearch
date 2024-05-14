28373853fe0e282581283562d374a8498e2746a5
Roman <rmayorov@gmail.com>
2024-05-14 20:15:35 +0400

    adding_html

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="wrapper">
+
+    <div class="container">
+        <div class="search-box">
+            <input class="search-input" type="text" placeholder="Введите запрос">
+            <button class="search-button">Поиск</button>
+        </div>
+
+        <WikiItem :title="title" :content="content" :date="date" :link="link"/>
+    </div>
+
+  </div>
+</template>
+
+
+
+<script setup>
+
+
+import { ref } from "vue";
+
+import WikiItem from "./components/WikiItem.vue";
+
+const title = "Заголовок статьи"
+const content = "Подзаголовок статьи"
+const date = "15.04.32"
+const link = "ffff"
+
+
+</script>
+
+
+
+
+<style scoped>
+  .wrapper{
+    max-width: 1280px;
+    margin: 0 auto;
+    font-weight: normal;
+    min-height: 100vh;
+  }
+
+  .container{
+    margin-top: 30px;
+    box-shadow: 0 4px 15px rgb(0 0 0 / 10%);
+    background-color: white;
+    min-height: 80vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 6%;
+  }
+
+  .search-box {
+    width: 90%;
+    margin-top: 30px;
+    display: flex;
+    gap: 10px;
+  }
+
+  .search-input{
+      flex: 1;
+      padding: 10px;
+      font-size: 16px;
+  }
+
+  .search-button{
+      padding: 10px 20px;
+      background-color: #007bff;
+      color: #fff;
+      border: none;
+      cursor: pointer;
+      font-size: 16px;
+  }
+
+  
+
+  
+
+
+</style>
---
+++ src/components/WikiItem.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="rezult">
+        <h2 class="rezult-title">{{ title }}</h2>
+        <p class="rezult-content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis optio, eum eius quod voluptates consequatur ad error tenetur quaerat cum odit temporibus in! Tenetur ullam expedita excepturi accusantium doloremque quas!</p>
+        <p class="rezult-date">{{ date }}</p>
+        <a class="rezult-link" :href="link">Ссылка на статью</a>
+    </div>
+</template>
+
+
+<script setup> 
+
+import {defineProps} from "vue";
+
+const props = defineProps(['title', 'content', 'date', 'link']);
+
+
+
+</script>
+
+
+<style>
+
+.rezult {
+      margin-top: 15px;
+      display: flex;
+      gap:15px;
+      max-width:500px;
+      flex-wrap:wrap;
+  }
+
+
+
+</style>
+
+
